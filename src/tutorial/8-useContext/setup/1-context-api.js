874fc4aa9c082397a8da5f8d9bd73e540e3b179a
tomix421q <zilka.tomas421@gmail.com>
2023-01-18 22:09:43 +0100

    st

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,46 +1,44 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, { useState, useContext, createContext } from 'react'
+import { data } from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = createContext()
+
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data)
   const removePerson = (id) => {
     setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
+      return people.filter((person) => person.id !== id)
+    })
+  }
   return (
-    <>
+    <PersonContext.Provider value={{ removePerson }}>
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
-};
+      <List people={people} />
+    </PersonContext.Provider>
+  )
+}
 
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+        return <SinglePerson key={person.id} {...person} />
       })}
     </>
-  );
-};
+  )
+}
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext)
+
   return (
     <div className='item'>
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
-  );
-};
+  )
+}
 
-export default ContextAPI;
+export default ContextAPI
